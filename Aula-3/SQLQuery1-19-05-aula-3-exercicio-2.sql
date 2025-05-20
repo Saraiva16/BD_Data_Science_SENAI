90f1aed8728e1e65b423a4b4fe7bbf6251d3228c
Saraiva16 <103701978+Saraiva16@users.noreply.github.com>
2025-05-19 21:43:02 -0300

    Add files via upload

---
+++ Aula-3/SQLQuery1-19-05-aula-3-exercicio-2.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE Cargos (
+COD_CARGO INT NOT NULL PRIMARY KEY,
+CARGO VARCHAR(530) NOT NULL,
+AREA VARCHAR(20)
+);
+
+INSERT INTO Cargos(COD_CARGO, CARGO, AREA) VALUES
+(1, 'Analista TI', 'Informática'),
+(2, 'Deisgner Gráfico', 'Marketing'),
+(3, 'Aux. Administrativo', 'Administração'),
+(4, 'Recepcionista', 'Administração'),
+(5, 'Vendedor', 'Marketing'),
+(6, 'Programador', 'Informática');
+
+SELECT * FROM Cargos;
+
+SELECT COUNT(COD_CARGO) FROM Cargos;
+
+SELECT DISTINCT AREA FROM Cargos;
+
+SELECT * FROM Cargos WHERE AREA like 'Marketing';
+
+SELECT * FROM Cargos ORDER BY CARGO ASC;
